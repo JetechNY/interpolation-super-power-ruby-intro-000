c76755510f3389619ccc6f0ce2c4325f8d6c2af0
SunJet Liu <sunjetliu@gmail.com>
2020-08-05 21:25:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,9 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   puts "#{colors[0]}"
+puts "#{colors[1]}"
+puts "#{colors[2]}"
+puts "#{colors[3]}"
+puts "#{colors[4]}"
 
 end
